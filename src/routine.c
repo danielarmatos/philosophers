013d886407dae8598f37a132f3345316721c3e66
Daniela Alexandra Reis Matos <dreis-ma@student.42porto.com>
2023-09-30 17:53:30 +0100

    Improved routine loop and start of the routine

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: dreis-ma <dreis-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 19:06:51 by dreis-ma          #+#    #+#             */
-/*   Updated: 2023/09/28 20:22:01 by dreis-ma         ###   ########.fr       */
+/*   Updated: 2023/09/30 17:34:58 by dreis-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,6 @@ void	p_think(t_data *data, int p_index)
 
 static void	p_eat_3(t_data *data, int p_index)
 {
-	if (data->philos[p_index]->meals_eaten == data->must_eat)
-	{
-		pthread_mutex_lock(&data->m_philos_full);
-		data->philos_full++;
-		pthread_mutex_unlock(&data->m_philos_full);
-	}
 	pthread_mutex_lock(&data->philos[p_index]->l_fork->m_fork);
 	data->philos[p_index]->l_fork->locked = false;
 	pthread_mutex_unlock(&data->philos[p_index]->l_fork->m_fork);
@@ -44,9 +38,15 @@ static void	p_eat_3(t_data *data, int p_index)
 	data->philos[p_index]->fork_amount = 0;
 	data->philos[p_index]->has_l_fork = false;
 	data->philos[p_index]->has_r_fork = false;
+	if (data->philos[p_index]->meals_eaten == data->must_eat)
+	{
+		pthread_mutex_lock(&data->m_philos_full);
+		data->philos_full++;
+		pthread_mutex_unlock(&data->m_philos_full);
+	}
 	p_sleep(data, p_index);
 	p_think(data, p_index);
-	usleep(100);
+	usleep(300);
 }
 
 static void	p_eat_2(t_data *data, int p_index)
@@ -58,13 +58,12 @@ static void	p_eat_2(t_data *data, int p_index)
 	if (data->philos[p_index]->has_l_fork == true
 		&& data->philos[p_index]->has_r_fork == true)
 	{
-		timestamp = get_timestamp(data);
 		print_message(data, p_index, 1);
-		data->philos[p_index]->meals_eaten++;
+		timestamp = get_timestamp(data);
 		data->philos[p_index]->status = EATING;
 		data->philos[p_index]->time_to_die = timestamp + data->time_die;
 		x = timestamp + data->time_eat;
-		if (x < data->philos[p_index]->time_to_die)
+		if (x <= data->philos[p_index]->time_to_die)
 			usleep(data->time_eat * 1000);
 		else
 		{
@@ -72,6 +71,7 @@ static void	p_eat_2(t_data *data, int p_index)
 			usleep(sleep_time * 1000);
 			p_die(data, p_index);
 		}
+		data->philos[p_index]->meals_eaten++;
 		p_eat_3(data, p_index);
 	}
 	else
@@ -85,13 +85,14 @@ void	p_eat(t_data *data, int p_index)
 	else
 	{
 		pthread_mutex_lock(&data->philos[p_index]->l_fork->m_fork);
-		pthread_mutex_lock(&data->philos[p_index]->r_fork->m_fork);
 		if (data->philos[p_index]->l_fork->locked == false)
 		{
 			data->philos[p_index]->l_fork->locked = true;
 			data->philos[p_index]->has_l_fork = true;
 			print_message(data, p_index, 0);
 		}
+		pthread_mutex_unlock(&data->philos[p_index]->l_fork->m_fork);
+		pthread_mutex_lock(&data->philos[p_index]->r_fork->m_fork);
 		if (data->philos[p_index]->has_l_fork == true
 			&& data->philos[p_index]->r_fork->locked == false)
 		{
@@ -99,7 +100,6 @@ void	p_eat(t_data *data, int p_index)
 			data->philos[p_index]->has_r_fork = true;
 			print_message(data, p_index, 0);
 		}
-		pthread_mutex_unlock(&data->philos[p_index]->l_fork->m_fork);
 		pthread_mutex_unlock(&data->philos[p_index]->r_fork->m_fork);
 	}
 	p_eat_2(data, p_index);
@@ -114,8 +114,14 @@ void	*routine(void *arg)
 	pthread_mutex_lock(&data->m_curr_id);
 	id = ++data->curr_id;
 	pthread_mutex_unlock(&data->m_curr_id);
+	if (id % 2 == 0)
+	{
+		p_think(data, id);
+		usleep(5000);
+	}
 	while (1)
 	{
+		p_eat(data, id);
 		pthread_mutex_lock(&data->m_dead);
 		if (check_if_dead(data) == true)
 		{
@@ -123,7 +129,6 @@ void	*routine(void *arg)
 			break ;
 		}
 		pthread_mutex_unlock(&data->m_dead);
-		p_eat(data, id);
 	}
 	return (0);
 }
